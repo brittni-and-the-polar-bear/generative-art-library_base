47cc9497c9ad1ad6d5739d09ba9c62199924d4a7
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-01-27 00:41:14 -0600

    Remove failure case.
    Failure case produced desired result.

--- src/test/math/math.test.ts
+++ src/test/math/math.test.ts
@@ -31,8 +31,7 @@ describe('math tests', (): void => {
         {row: 1, column: 2, totalRows: 1, totalColumns: 3, expected: undefined},
         {row: 1, column: 5, totalRows: 4, totalColumns: 3, expected: undefined},
         {row: 5, column: 5, totalRows: 4, totalColumns: 3, expected: undefined},
-        {row: 3, column: 2, totalRows: 4, totalColumns: 3, expected: 11},
-        {row: 3, column: 2, totalRows: 4, totalColumns: 3, expected: undefined} // failure test. remove later.
+        {row: 3, column: 2, totalRows: 4, totalColumns: 3, expected: 11}
     ])('$# getSingleDimensionIndex($row, $column, $totalRows, $totalColumns)',
         ({row,
              column,
